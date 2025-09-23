55afaf89ecc688811195341c18e640fb8f05da2a
anjanaphegde <anjanahegdee21@gmail.com>
2025-09-23 16:39:59 +0530

    Find the Maximm element in an array.

--- Day_4/app.js
+++ Day_4/app.js
@@ -1,20 +1,46 @@
-//1. Find the sum of elements in an array
-//Method 1: Using for loop
-function sumArr(arr){
-    let sum = 0;
-    for(let i = 0; i < arr.length; i++){
-        sum += arr[i];
+// //1. Find the sum of elements in an array
+// //Method 1: Using for loop
+// function sumArr(arr){
+//     let sum = 0;
+//     for(let i = 0; i < arr.length; i++){
+//         sum += arr[i];
+//     }
+//     return sum;
+// }
+// console.log(sumArr([2,54,12,71,34,78]));
+
+// //Method 2: Using reduce method
+
+// function sumArr2(arr1){
+//     return arr1.reduce((acc, curr) => {
+//         return acc + curr;
+//     }, 0);
+// }
+
+// console.log(sumArr2([2,54,12,71,34,78]));
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// 2. Find the Maximm element in an array.
+//Method 1: Using reduce method
+
+function findMax(arr){
+    const max =  arr.reduce((acc, curr)=>{
+        return acc > curr ? acc : curr;
+    }, arr[0]);
+    return max;
+}
+console.log(findMax([2,54,12,71,34,78]));
+
+//Method 2: Using looping
+function finMax1(arr1){
+    let max = arr1[0];
+    for(let i = 0; i < arr1.length; i++){
+        if(arr1[i] > max) {
+            max = arr1[i];
+        }
     }
-    return sum;
-}
-console.log(sumArr([2,54,12,71,34,78]));
-
-//Method 2: Using reduce method
-
-function sumArr2(arr1){
-    return arr1.reduce((acc, curr) => {
-        return acc + curr;
-    }, 0);
+    return max;
 }
 
-console.log(sumArr2([2,54,12,71,34,78]));
+console.log(finMax1([2,54,12,71,34,78]));
