fa18c608a53dad3b885994b713baab48b376fd34
anjanaphegde <anjanahegdee21@gmail.com>
2025-09-25 09:27:06 +0530

    Reverse an array with 4 methods

--- Day_4/app.js
+++ Day_4/app.js
@@ -48,18 +48,85 @@
 
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-// find the second max element in a given array.
+// 3. find the second max element in a given array.
 
-let arr = [28, 6, 74, 48, 84, 79];
-let max = Math.max(arr[0], arr[1]);
-let sMax = Math.min(arr[0], arr[1]);
+// let arr = [28, 6, 74, 48, 84, 79];
+// let max = Math.max(arr[0], arr[1]);
+// let sMax = Math.min(arr[0], arr[1]);
 
-for(let i = 2; i< arr.length; i++){
-    if(arr[i] > max){
-        sMax = max;
-        max = arr[i];
-    } else if(arr[i] > sMax && arr[i] != max){
-        sMax = arr[i];
-    }
+// for(let i = 2; i< arr.length; i++){
+//     if(arr[i] > max){
+//         sMax = max;
+//         max = arr[i];
+//     } else if(arr[i] > sMax && arr[i] != max){
+//         sMax = arr[i];
+//     }
+// }
+// console.log('second max is: '+ sMax);
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//4. Find the 2nd min element in a given array
+
+// let arr = [28, 6, 74, 48, 84, 79];
+// let min = Math.min(arr[0], arr[1]);
+// let sMin = Math.max(arr[0 ], arr[1]);
+
+// for(let i = 2; i < arr.length; i++){
+//     if( arr[i] < min){
+//         sMin = min;
+//         min = arr[i];
+//     } else if(arr[i] < sMin && arr) {
+//         sMin = arr[i];
+//     }
+// }
+// console.log('second min is: ' + sMin);
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// // 5. Reverse an array
+//method 1: using temp array
+// let arr = [1,2,3,4,5,6];
+
+// let temp = new Array(arr.length);
+// let i = 0;
+// for(let j = arr.length - 1; j >= 0; j--) {
+//     temp[i] = arr[j];
+//     i++;
+// }
+// console.log(temp);
+
+
+// method 2: two pointer swap
+
+// let arr = [1,2,3,4,5,6];
+// let left = 0, right =arr.length - 1;
+
+// while(left < right) {
+//     [arr[left], arr[right]] = [arr[right], arr[left]];
+//     left ++;
+//     right --;
+// }
+// console.log(arr);
+
+//Method 3: Simple Loop with Push
+
+// let arr = [1,2, 3,4,5,6];
+// let temp = [];
+// for(let i = arr.length - 1; i >= 0; i--){
+//     temp.push(arr[i]);
+// }
+// console.log(temp);
+
+//Method 4: 
+let arr = [1,2,3,4,5,6];
+let i = 0, j = arr.length - 1;
+
+while(i < j) {
+    temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+    i++;
+    j--;
 }
-console.log('second max is: '+ sMax);
+console.log(arr);
