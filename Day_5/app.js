03b2f8317442eec6e6874587a6ab93e79715a0c6
anjanaphegde <anjanahegdee21@gmail.com>
2025-09-28 17:46:49 +0530

    day-5

--- Day_5/app.js
+++
@@ -1,9 +0,0 @@
-// Reverse an array using two pointer approach.
-let arr = [1,2,3,4,5];
-let left = 0, right = arr.length -1;
-while(left < right) {
-    [arr[left], arr[right]] = [arr[right], arr[left]];
-    left++;
-    right--;
-}
-console.log(arr);
