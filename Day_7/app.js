a56658307a4e4c622e66744728ee31644e202e0f
anjanaphegde <anjanahegdee21@gmail.com>
2025-10-09 12:10:57 +0530

    Day 7

--- Day_7/app.js
+++ Day_7/app.js
@@ -1,29 +1,52 @@
-// check palindrome number.
+// // check palindrome number.
 
-//Approach 1: String Converion
+// //Approach 1: String Converion
 
-function isArrPalindrome(arr){
-    let s = 0, e = arr.length - 1;
-    while(s < e){
-        if(arr[s] !== arr[e]) return false;
-        s++;
-        e--;
+// function isArrPalindrome(arr){
+//     let s = 0, e = arr.length - 1;
+//     while(s < e){
+//         if(arr[s] !== arr[e]) return false;
+//         s++;
+//         e--;
+//     }
+//     return true;
+// }
+// console.log(isArrPalindrome([0, 1, 2, 1, 0]));
+// console.log(isArrPalindrome([0, 1, 2, 3, 0]));
+
+// // Time Complexity -> o(n)
+// // Space Complexity -> o(1)
+
+
+
+// // approach 2: Reverse the array and compare ( simple but not optimal)
+
+//  function isArrpalindrom2(arr2) {
+//     return JSON.stringify(arr2) === JSON.stringify(arr2.slice().reverse());
+//  }
+//  console.log(isArrpalindrom2([0, 1, 2, 1, 0]));
+//  console.log(isArrpalindrom2([0, 1, 2, 3, 0]));
+
+//-----------------------------------------------------------------------------------------------------------------
+
+// Find the second Maximum element in array
+
+let arr = [28, 6, 74, 48, 84, 79];
+let max = -Infinity;   // start with the lowest possible value
+let sMax = -Infinity;  // second max also starts at lowest
+
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+        // if current element is greater than max
+        sMax = max;     // old max becomes second max
+        max = arr[i];   // update max
+    } 
+    else if (arr[i] > sMax && arr[i] !== max) {
+        // if current element is between max and sMax
+        sMax = arr[i];
     }
-    return true;
 }
-console.log(isArrPalindrome([0, 1, 2, 1, 0]));
-console.log(isArrPalindrome([0, 1, 2, 3, 0]));
 
+console.log("Second maximum element is:", sMax);
 // Time Complexity -> o(n)
 // Space Complexity -> o(1)
-
-
-
-// approach 2: Reverse the array and compare ( simple but not optimal)
-
- function isArrpalindrom2(arr2) {
-    return JSON.stringify(arr2) === JSON.stringify(arr2.slice().reverse());
- }
- console.log(isArrpalindrom2([0, 1, 2, 1, 0]));
- console.log(isArrpalindrom2([0, 1, 2, 3, 0]));
-
