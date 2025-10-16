9a16eb1a3f0b217bb167df14b33b5c8dcb62546f
anjanaphegde <anjanahegdee21@gmail.com>
2025-10-16 16:52:18 +0530

    feat: implement bubble sort algorithm with optimizations
    Add two implementations of bubble sort algorithm:
    1. Basic version with nested loops
    2. Optimized version using swap flag to exit early when array is sorted

---
+++ Day_8/app.js
@@ -0,0 +1,50 @@
+//Bubble sort : 
+//Time complexity : O(n^2)
+//Space complexity : O(1)
+//definition : It's a simple sorting algorithm that repeatedly steps through the list,
+// compares adjacent elements and swaps them if they are in wrong order.
+// The pass through the list is repeated until the list is sorted.
+
+//Approach 1: Not efficient but simple.
+
+function bubbleSort(arr){
+    for(let i = 0; i< arr.length - 1; i++) {
+        for(let j = 0; j < arr.length - 1 - i; j++){
+            if(arr[j] > arr[j+1]) {
+                let temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+console.log(bubbleSort([6, 3, 1, 8, 12, 2, 5]));
+
+//here we have used -i in inner loop because after each pass through the list,
+// the largest element will be at the end of the list, so we don't need to compare it again.
+
+//Approach 2: optimized version using flag 
+//Time complexity : O(n^2)
+//Space complexity : O(1)
+//definition : It's a simple sorting algorithm that repeatedly steps through the list,
+// compares adjacent elements and swaps them if they are in wrong order.
+// The pass through the list is repeated until the list is sorted.
+//In this approach we are using a flag to check if any swap is done in a pass through the list.
+//If no swap is done, then the list is sorted.
+
+function bubbleSort1(arr1){
+    let swapped;
+    for(let i = 0; i <arr1.length - 1; i++){
+        swapped = false;
+        for(let j = 0; j < arr1.length - 1 - i; j++){
+            if(arr1[j] > arr1[j+1]){
+                [arr1[j], arr1[j+1]] = [arr1[j+1], arr1[j]];
+                swapped = true;
+            }
+        }
+        if(!swapped) break;
+    }
+    return arr1;
+}
+console.log(bubbleSort1([6, 22, 1, 8, 12, 2, 5]));
