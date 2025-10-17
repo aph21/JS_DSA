6885b8ff7d97d2e837c7d0c37788bec8cc3de7e6
anjanaphegde <anjanahegdee21@gmail.com>
2025-10-17 17:13:55 +0530

    feat: add function to find 3rd maximum number in array
    Implement thirdMax function that finds the 3rd distinct maximum number in an array. If there are fewer than 3 distinct numbers, it returns the maximum. Includes test cases for validation.

---
+++ Day_9/app.js
@@ -0,0 +1,11 @@
+//Find 3rd maximum number in an array
+
+function thirdMax(nums){
+    let distinct = new Set(nums);
+    let arr = Array.from(distinct).sort((a,b) => b-a);
+    if(arr.length >= 3) return arr[2];
+    return arr[0];
+}
+console.log(thirdMax([3,2,1]));
+console.log(thirdMax([1,2]));
+console.log(thirdMax([2,2,3,1]));
